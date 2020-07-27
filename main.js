037ed11ed5b5fe7cd5856c0c2147117fe16f0326
Marina8989 <marina.mill209@gmail.com>
2020-07-26 18:25:14 -0700

    added some html

---
+++ main.js
@@ -0,0 +1,10 @@
+// Part 1
+
+// For this assignment you will be combining your knowledge of DOM access and events to build a todo app!
+
+// As a user, you should be able to:
+
+// Add a new todo(by submitting a form)
+// Mark a todo as completed (cross out the text of the todo)
+// Remove a todo
+
