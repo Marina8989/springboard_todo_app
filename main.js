341cd39a60af7cd141dc0bd6b7dd9b20f2a0b067
Marina8989 <marina.mill209@gmail.com>
2020-08-12 23:12:05 -0700

    todo app

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const todoForm = document.querySelector('#newTodoForm');
 const todoList = document.querySelector('#todoList');
+const savedTodo = JSON.parse(localStorage.getItem('todos')) || [];
 
 todoForm.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -7,7 +8,24 @@ todoForm.addEventListener('submit', function(e) {
     const newTodo = document.createElement('li');
     const inputValue = document.querySelector('#text').value;
     newTodo.innerText = inputValue;
+    newTodo.isCompleted = false;
     todoForm.reset();
-
+    
     todoList.appendChild(newTodo);
+
+    savedTodo.push({task: newTodo.iinerText, isCompleted: false});
+    localStorage.setItem('todos', JSON.stringify(savedTodo));
+})
+
+todoList.addEventListener('click', function(e) {
+    let completedTodo = e.target;
+   
+    if(!completedTodo.isCompleted) {
+       completedTodo.style.textDecoration = 'line-through';
+       completedTodo.isCompleted = true;
+    }else {
+       completedTodo.style.textDecoration = "none";
+       completedTodo.isCompleted = false;
+    }
+
 })
